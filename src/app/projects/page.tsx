cbf878fd647122261cddbd66fef9925b6cf7439a
Morgan James <mj522@exeter.ac.uk>
2024-08-27 01:27:51 +0100

    building about , contact and resdesigning home

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import Projects from "@/components/ContactDetails";
+import MyProjects from "@/components/MyProjects";
+
+export default function ProjectDisplay() {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-center mt-8">Contact Me</h1>
+            <MyProjects />
+        </div>
+    );
+}
+
---
+++ src/components/MyProjects.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+
+function MyProjects() {
+    return (
+
+        <h1>
+            <Navbar />
+            hello
+        </h1>
+    );
+}
+
+export default MyProjects;
