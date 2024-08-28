2351d6703e781b96aba22471562e5e98d298205d
Morgan James <mj522@exeter.ac.uk>
2024-08-28 19:09:05 +0100

    improving style and adding download cv button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,9 +42,27 @@ export default function Home() {
                             <img
                                 src="/linkedin.png"
                                 alt="Visit My LinkedIn"
-                                className="h-6 w-auto" // Adjust height as needed
+                                className="h-5 w-auto" // Adjust height as needed
                             />
                         </a>
+                        <a href={"/MorganJamesCV.pdf"} download="Morgan_James_CV.pdf" >
+                            <button
+                                type="button"
+                                className="inline-flex items-center gap-1.5 rounded-lg border border-primary-500 bg-white px-5 py-2.5 text-center text-sm font-bold text-black shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-4 w-4"
+                                >
+                                    <path d="M10.75 2.75a.75.75 0 00-1.5 0v8.614L6.295 8.235a.75.75 0 10-1.09 1.03l4.25 4.5a.75.75 0 001.09 0l4.25-4.5a.75.75 0 00-1.09-1.03l-2.955 3.129V2.75z" />
+                                    <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z" />
+                                </svg>
+                                My CV
+                            </button>
+                        </a>
+
                     </div>
                     <div className="text-left mt-6">
                         <h1 className="text-2xl mb-3 text-gray-500 dark:text-gray-400">Welcome to my Portfolio</h1>
