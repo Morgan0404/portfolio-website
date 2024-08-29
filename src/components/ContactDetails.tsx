3e88d50ca24156231ac2aeab719843c38bbedfbf
Morgan James <mj522@exeter.ac.uk>
2024-08-29 07:15:33 +0100

    improving contact layout

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -2,6 +2,7 @@ import DownloadCVButton from "./DownloadCVButton";
 import navbar from "@/components/Navbar";
 import Navbar from "@/components/Navbar";
 import React from "react";
+import Image from "next/image";
 function ContactDetails() {
 
     return (
@@ -48,7 +49,7 @@ function ContactDetails() {
                         </div>
 
                         <button type="button"
-                                className="mb-6 inline-block w-full rounded bg-teal-400 px-6 py-2.5 font-medium uppercase leading-normal text-white hover:shadow-md hover:bg-teal-500">
+                                className="mb-6 inline-block w-full rounded bg-blue-600 px-6 py-2.5 font-medium uppercase leading-normal text-white hover:shadow-md hover:bg-teal-500">
                             Send
                         </button>
 
@@ -69,13 +70,13 @@ function ContactDetails() {
                                     </div>
                                     <div className="ml-6 grow">
                                         <p className="mb-2 font-bold">
-                                            Technical support
+                                            Contact me
                                         </p>
                                         <p className="text-neutral-500 ">
-                                            support@example.com
+                                            morganjames092@gmail.com
                                         </p>
                                         <p className="text-neutral-500 ">
-                                            +1 234-567-89
+                                            07710466903
                                         </p>
                                     </div>
                                 </div>
@@ -99,7 +100,7 @@ function ContactDetails() {
                                             sales@example.com
                                         </p>
                                         <p className="text-neutral-500 ">
-                                            +1 234-567-89
+
                                         </p>
                                     </div>
                                 </div>
@@ -121,7 +122,7 @@ function ContactDetails() {
                                             press@example.com
                                         </p>
                                         <p className="text-neutral-500 ">
-                                            +1 234-567-89
+
                                         </p>
                                     </div>
                                 </div>
@@ -145,11 +146,61 @@ function ContactDetails() {
                                             bugs@example.com
                                         </p>
                                         <p className="text-neutral-500">
-                                            +1 234-567-89
+
                                         </p>
                                     </div>
+
+
                                 </div>
                             </div>
+                        </div>
+                        <div className="flex gap-7 justify-center mt-7">
+                            <a
+                                href="https://github.com/Morgan0404"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="bg-white p-2 rounded-md flex items-center justify-center"
+                            >
+                                <Image
+                                    src="/githublogo.png"
+                                    alt="Visit My GitHub"
+                                    width="80"
+                                    height = "7"
+                                    className="h-7 w-20" // Adjust height as needed
+                                />
+                            </a>
+                            <a href={"/MorganJamesCV.pdf"} download="Morgan_James_CV.pdf" >
+                                <button
+                                    type="button"
+                                    className="inline-flex items-center gap-1.5 rounded-lg border border-primary-500 bg-white px-5 py-2.5 text-center text-sm font-bold text-black shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        className="h-7 w-4"
+                                    >
+                                        <path d="M10.75 2.75a.75.75 0 00-1.5 0v8.614L6.295 8.235a.75.75 0 10-1.09 1.03l4.25 4.5a.75.75 0 001.09 0l4.25-4.5a.75.75 0 00-1.09-1.03l-2.955 3.129V2.75z" />
+                                        <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z" />
+                                    </svg>
+                                    My CV
+                                </button>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/morgan-james-09092b295/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="bg-white p-2 rounded-md flex items-center justify-center"
+                            >
+                                <Image
+                                    src="/linkedin.png"
+                                    alt="Visit My LinkedIn"
+                                    width="80"
+                                    height="5"
+                                />
+                            </a>
+
+
                         </div>
                     </div>
 
