fe65a751fb015d6e44ca567fd60b85d9decd9d47
Morgan James <mj522@exeter.ac.uk>
2024-08-31 22:44:31 +0100

    starting initial draft of projects page v4

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -34,15 +34,38 @@ export default function MyProjects() {
                     <Panel
                         imageSrc="/githublogo.png" // Replace with your image path
                         imageAlt="Project Image 2"
-                        heading="PROJECT 2 TITLE"
-                        text="This is the description for project 2. The image is on the right."
+                        heading="Grad Gig: Full stack web application"
+                        text={
+                            <>
+                                <p>
+                                    I successfully developed and deployed a full-stack job board application utilising the latest Next.js framework, incorporating server components, and integrating a MongoDB database. This project serves as a comprehensive demonstration of my full-stack development capabilities, from front-end design to back-end implementation and database management. The application includes key features such as user authentication, company creation, job posting with image uploads to S3, and CRUD operations for job listings.
+                                </p>
+                                <br/>
+                                <p>
+                                    For authentication, I integrated WorkOS, enabling seamless and secure user login and management. The project was built with a focus on providing a seamless user experience and leveraging modern web technologies. I covered all essential aspects from the homepage setup to handling job data within the MongoDB database. This project showcases my ability to build robust, scalable web applications, with the entire application deployed on Vercel.
+                                </p>
+                            </>
+                        }
                         reverse={true}
                     />
                     <Panel
                         imageSrc="/githublogo.png" // Replace with your image path
                         imageAlt="Project Image 3"
-                        heading="PROJECT 3 TITLE"
-                        text="This is the description for project 3. The image is on the left."
+                        heading="Sustain-A-Meal"
+                        text={
+                            <>
+                            <p>
+                                I developed and deployed Sustain A Meal as part of a group software development project aimed at combating food waste and promoting sustainability among students. The application leverages AI through OpenAI integration to generate recipes based on available ingredients, serving size, and allergies, helping students minimise food waste. The project included a gamification element where users earned points for reducing waste, fostering competition among students and accommodation teams.
+                            </p>
+                                <br/>
+                                <p>
+                                    Authentication was securely managed with Clerk, and we employed a robust CI/CD pipeline with Docker Compose and NGINX for deployment. Version control was handled using Git, with collaboration facilitated through a Github organisation and automated processes using GitHub Actions. This project showcases my ability to work effectively in a team, utilising modern development practices and tools to deliver a meaningful, sustainable solution
+                                </p>
+
+                            </>
+
+
+                        }
                         reverse={false}
                     />
                 </div>
