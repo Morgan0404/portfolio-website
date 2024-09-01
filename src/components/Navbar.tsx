b5d70ba994567dca84043abb8d0bbac4ecaaceca
Morgan James <mj522@exeter.ac.uk>
2024-09-01 20:04:48 +0100

    starting initial development of about page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -70,20 +70,21 @@ const Navbar = () => {
                         </li>
                         <li>
                             <Link
-                                href="/projects"
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                Projects
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
                                 href="/about"
                                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                             >
                                 About me
                             </Link>
                         </li>
+                        <li>
+                            <Link
+                                href="/projects"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Projects
+                            </Link>
+                        </li>
+
 
                         <li>
                             <Link
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,20 +2,48 @@ import React from 'react';
 
 const Timeline = () => {
     return (
-        <ol className="relative border-s border-gray-200 dark:border-gray-700">
+        <div className="text-left mt-6 p-6 border border-gray-200 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800">
+
+        <ol className=" relative border-l-4 border-blue-600 dark:border-blue-700">
+
             <li className="mb-10 ms-4">
                 <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                 <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    February 2022
+                    September 2021 - September 2022
                 </time>
                 <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                    Application UI code in Tailwind CSS
+                    Software Development instructor (Code ninjas)
+                </h3>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                    While studying in college, I worked at Code Ninjas, where I developed over 20 curriculum modules, including 10 comprehensive lesson plans focused on software development practices like data structures, algorithms, and design patterns. I guided students through coding projects, helping them apply essential principles such as debugging, version control, and iterative development. Additionally, I led both on-site and virtual workshops on game development and software design, providing interactive environments that reinforced practical coding skills.
+                </p>
+            </li>
+
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                    April 2022 - Present
+                </time>
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                    Excode - Python workshop leader (Exeter University)
+                </h3>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+                    While studying at university, I instructed over 30 students in Python, leading workshops that covered everything from basic syntax to advanced topics like object-oriented programming and algorithm design. I also developed and implemented curriculum materials, focusing on essential software development practices, including data structures and algorithm design, ensuring students gained a strong foundation in these key areas.
+                </p>
+            </li>
+            <li className="mb-10 ms-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                    September 2023 - Present
+                </time>
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                    Google Student Developer Club - member
                 </h3>
                 <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-                    Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.
+                    As a member of the Google Developer Student Clubs (GDSC), I have had the opportunity to immerse myself in a vibrant community of aspiring developers and tech enthusiasts. Since joining, I have actively participated in various talks, workshops, and hackathons, all of which have significantly contributed to my growth as a developer. The experience has not only enhanced my technical skills but also fostered strong teamwork abilities, as I collaborated with peers on real-world projects and challenges. Being part of GDSC has been instrumental in my journey, providing me with the resources and support to continually evolve in the ever-changing field of software development.
                 </p>
                 <a
-                    href="#"
+                    href="/projects"
                     className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                 >
                     Learn more
@@ -36,31 +64,9 @@ const Timeline = () => {
                     </svg>
                 </a>
             </li>
-            <li className="mb-10 ms-4">
-                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    March 2022
-                </time>
-                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                    Marketing UI design in Figma
-                </h3>
-                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                    All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.
-                </p>
-            </li>
-            <li className="ms-4">
-                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    April 2022
-                </time>
-                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                    E-Commerce UI code in Tailwind CSS
-                </h3>
-                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                    Get started with dozens of web components and interactive elements built on top of Tailwind CSS.
-                </p>
-            </li>
+
         </ol>
+        </div>
     );
 };
 
