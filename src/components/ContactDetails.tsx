cd9151b992c460b46d044e62d860d4177388cbaf
Morgan James <mj522@exeter.ac.uk>
2024-09-11 01:50:45 +0100

    improving contact page

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -11,7 +11,7 @@ function ContactDetails() {
             <section className="fade-in mb-20 pt-9">
                 <div className=" flex justify-center">
                     <div className="text-center mb-10 md:max-w-xl lg:max-w-3xl">
-                        <h2 className="mb-1 px-6 text-3xl font-bold">
+                        <h2 className="mb-1 px-6 py-10 text-3xl font-bold">
                             Contact Me
                         </h2>
                     </div>
@@ -71,7 +71,7 @@ function ContactDetails() {
                                     </div>
                                     <div className="ml-6 grow">
                                         <p className="mb-2 font-bold">
-                                            Contact me
+                                            Personal Contact
                                         </p>
                                         <p className="text-neutral-500 ">
                                             morganjames092@gmail.com
@@ -97,69 +97,19 @@ function ContactDetails() {
                                     </div>
                                     <div className="ml-6 grow">
                                         <p className="mb-2 font-bold ">
-                                            Sales questions
+                                            Internship enquiry
                                         </p>
                                         <p className="text-neutral-500 ">
-                                            sales@example.com
+                                            morganjames202004@gmail.com
                                         </p>
                                         <p className="text-neutral-500 ">
-
+                                            07710466903
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
-                                <div className="align-start flex">
-                                    <div className="shrink-0">
-                                        <div className="inline-block rounded-md bg-blue
--400-100 p-4 text-blue
--700">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 strokeWidth="2" stroke="currentColor" className="h-6 w-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                      d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"/>
-                                            </svg>
-                                        </div>
-                                    </div>
-                                    <div className="ml-6 grow">
-                                        <p className="mb-2 font-bold ">Press</p>
-                                        <p className="text-neutral-500 ">
-                                            press@example.com
-                                        </p>
-                                        <p className="text-neutral-500 ">
-
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
-                                <div className="align-start flex">
-                                    <div className="shrink-0">
-                                        <div className="inline-block rounded-md bg-blue
--400-100 p-4 text-blue
--700">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 strokeWidth="2" stroke="currentColor" className="h-6 w-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round"
-                                                      d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0112 12.75zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 01-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 002.248-2.354M12 12.75a2.25 2.25 0 01-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 00-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 01.4-2.253M12 8.25a2.25 2.25 0 00-2.248 2.146M12 8.25a2.25 2.25 0 012.248 2.146M8.683 5a6.032 6.032 0 01-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0115.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 00-.575-1.752M4.921 6a24.048 24.048 0 00-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 01-5.223 1.082"/>
-                                            </svg>
-                                        </div>
-                                    </div>
-                                    <div className="ml-6 grow">
-                                        <p className="mb-2 font-bold">
-                                            Bug report
-                                        </p>
-                                        <p className="text-neutral-500 ">
-                                            bugs@example.com
-                                        </p>
-                                        <p className="text-neutral-500">
-
-                                        </p>
-                                    </div>
 
 
-                                </div>
-                            </div>
                         </div>
                         <div className="flex gap-7 justify-center mt-7">
                             <a
