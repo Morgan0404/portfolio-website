aa74a055e358422e2b7a1a5d4e241f35db92a426
Morgan James <mj522@exeter.ac.uk>
2024-09-14 01:12:26 +0100

    making mobile compatible landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,9 @@ export default function Home() {
                         {/* CV Button */}
                         <a href={"/MorganJamesCVnewfinal.pdf"} download="MorganJamesCVnewfinal.pdf">
                             <button
+
+                                style={{ width: '112px', height: '42px' }}
+
                                 type="button"
                                 className="fade-in w-30 h-19.5 inline-flex items-center gap-1.5 rounded-lg border border-primary-500 bg-white px-8 py-2.5 text-center text-sm font-bold text-black shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300"
                             >
