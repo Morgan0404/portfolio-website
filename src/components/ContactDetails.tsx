3651296b4c96aeb697746f384512bb1678e43652
Morgan James <mj522@exeter.ac.uk>
2024-09-16 00:04:23 +0100

    improving ui design for phone compatability v3

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -118,24 +118,21 @@ function ContactDetails() {
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="bg-white p-2 rounded-md flex items-center justify-center"
-
                             >
                                 <Image
                                     src="/githublogo.png"
                                     alt="Visit My GitHub"
                                     width="80"
-                                    height = "7"
-                                    className="h-7 w-20" // Adjust height as needed
+                                    height="7"
+                                    className="h-7 w-20"
                                 />
                             </a>
-                            <a href={"/MorganJamesCVnewfinal.pdf"} download="MorganJamesCVnewfinal.pdf" >
+                            <a href={"/MorganJamesCVnewfinal.pdf"} download="MorganJamesCVnewfinal.pdf">
                                 <button
                                     type="button"
-                                    className="inline-flex items-center gap-1.5 rounded-lg border border-primary-500 bg-white px-5 py-2.5 text-center text-sm font-bold text-black shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300"
-                                    style={{ width: '118px', height: '41px' }}  // Fixed size matching image
-
+                                    className="inline-flex items-center gap-1.5 rounded-lg border border-primary-500 bg-white px-5 py-2.5 text-sm font-bold text-black shadow-sm hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200"
+                                    style={{ width: "118px", height: "41px" }}
                                 >
-
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20"
@@ -145,23 +142,16 @@ function ContactDetails() {
                                         <path d="M10.75 2.75a.75.75 0 00-1.5 0v8.614L6.295 8.235a.75.75 0 10-1.09 1.03l4.25 4.5a.75.75 0 001.09 0l4.25-4.5a.75.75 0 00-1.09-1.03l-2.955 3.129V2.75z" />
                                         <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z" />
                                     </svg>
-
                                     My CV
                                 </button>
                             </a>
                             <a
-
                                 href="https://www.linkedin.com/in/morgan-james-09092b295/"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="bg-white p-2 rounded-md flex items-center justify-center"
                             >
-                                <Image
-                                    src="/linkedin.png"
-                                    alt="Visit My LinkedIn"
-                                    width="80"
-                                    height="5"
-                                />
+                                <Image src="/linkedin.png" alt="Visit My LinkedIn" width="80" height="5" />
                             </a>
 
 
